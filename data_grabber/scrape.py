32316f6376e5e20569256942057bbd582e124ec7
Zack Cooper <zackjcooper@gmail.com>
2015-04-07 16:18:44 -0400

    index is populating qb info

--- data_grabber/scrape.py
+++ data_grabber/scrape.py
@@ -1,6 +1,6 @@
 import http.client
 from bs4 import BeautifulSoup
-from ..models import Player, Passing, Game
+from .models import Player, Passing, Game
 import re
 
 
@@ -21,34 +21,7 @@ def url_request(partial_path):
         return soup_object
 
 
-def lat_long_lookup(bsoup_item):
-    location = bsoup_item.find('div', attrs={'class' : 'viewposting'})
-    if location:
-        item_lat = location["data-latitude"].split("data-latitude=")[-1]
-        item_long = location["data-longitude"].split("data-longitude=")[-1]
-        return item_lat, item_long
-    else:
-        return (None, None)
-
-
-def item_lookup(bsoup_item):
-
-    ind_page = url_request(bsoup_item)
-    souped_item = BeautifulSoup(ind_page)
-
-    item_name = name_lookup(souped_item)
-    item_url = 'http://raleigh.craigslist.com{}'.format(bsoup_item)
-    item_dates = date_lookup(souped_item)
-    item_images = image_lookup(souped_item)
-    item_lat, item_long = lat_long_lookup(souped_item)
-    item_description = description_lookup(souped_item)
-
-    return (item_name, item_url, item_dates, item_images,
-            item_lat, item_long, item_description)
-
-
 def initial_player_lookup(position='qb'):
-
     position_url = {'qb':'PTDS', 'rb':'RYDS', 'wr':'RECTDS'}
     position_path = 'stats/leaders/NCAAF/' + position_url[position] + \
                                             '/regularseason?&start_row=1'
@@ -56,34 +29,42 @@ def initial_player_lookup(position='qb'):
     all_players = souped_page.find_all('tr', attrs={'class': 'row2'})
     all_players.extend(souped_page.find_all('tr', attrs={'class': 'row1'}))
     for player in all_players:
-        ext_id = re.sub(r'\D', "", player.find('a')['href'])
+        save_player_data(player, position)
 
-        if not Player.query.filter_by(ext_id=ext_id).first():
-            each_player = player.find_all('td')
-            player = Player(ext_id=ext_id,
-                            name=each_player[1].text,
-                            team=each_player[2].text,
-                            position=position,
-                            year=2014)
-            db.session.add(player)
-    db.session.commit()
+
+def save_player_data(bs_chunk, position):
+    ext_id = re.sub(r'\D', "", bs_chunk.find('a')['href'])
+    if not Player.objects.filter(ext_id=ext_id):
+        each_player = bs_chunk.find_all('td')
+        new = Player(ext_id=ext_id,
+                        name=each_player[1].text,
+                        team=each_player[2].text,
+                        position=position,
+                        year=2014)
+        new.save()
+
+
+
+
+
 
 def quarterback_stat_lookup(player):
     player_name = player.name.replace(' ', '-')
     souped_page = url_request('players/playerpage/' + str(player.ext_id) + '/' + player_name)
 
     all_stats = souped_page.find_all('tr', attrs={'class': 'row1'})
-    all_stats.extend(souped_page.find_all('tr', attrs={'class': 'row1'})
+    all_stats.extend(souped_page.find_all('tr', attrs={'class': 'row1'}))
 
     for stat in all_stats:
         each_stat = stat.find_all('td')
         print(each_stat)
         score = score_breakout(each_stat[2].text)
-        game_exists = Game.query.filter_by(team=player.team,
-                                           game_date=each_stat[0].text,
-                                           opponent=each_stat[1].text,
-                                           your_score=score[0],
-                                           opponent_score=score[1]).first()
+        print(score)
+        game_exists = Game.objects.get(team=player.team,
+                                       game_date=each_stat[0].text,
+                                       opponent=each_stat[1].text,
+                                       your_score=score[0],
+                                       opponent_score=score[1]).first()
         if not game_exists:
             game_stats = Game(team=player.team,
                               game_date=each_stat[0].text,
@@ -96,12 +77,11 @@ def quarterback_stat_lookup(player):
                                  completions=each_stat[4].text,
                                  yards=each_stat[6].text,
                                  touchdowns=each_stat[8].text)
-            db.session.add(pass_stats)
-            db.session.add(game_stats)
-    db.session.commit()
+            pass_stats.save()
+            game_stats.save()
 
 def lookup_specific_stats(player_position):
-    all_players = Player.query.filter_by(position=player_position)
+    all_players = Player.objects.filter(position=player_position)
     for player in all_players:
         quarterback_stat_lookup(player)
 
@@ -109,3 +89,27 @@ def score_breakout(score):
     scores = score.split('-')
     cleaned = [score.replace('W\xa0', '') for score in scores]
     return cleaned
+
+
+
+
+
+
+
+
+
+
+#Not using
+def item_lookup(bsoup_item):
+    ind_page = url_request(bsoup_item)
+    souped_item = BeautifulSoup(ind_page)
+
+    item_name = name_lookup(souped_item)
+    item_url = 'http://raleigh.craigslist.com{}'.format(bsoup_item)
+    item_dates = date_lookup(souped_item)
+    item_images = image_lookup(souped_item)
+    item_lat, item_long = lat_long_lookup(souped_item)
+    item_description = description_lookup(souped_item)
+
+    return (item_name, item_url, item_dates, item_images,
+        item_lat, item_long, item_description)
--- data_grabber/views.py
+++ data_grabber/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+
+from .scrape import initial_player_lookup, lookup_specific_stats
+from .models import Player
+
+
+def index(request):
+    context = {'players': Player.objects.all()}
+    initial_player_lookup()
+    lookup_specific_stats('qb')
+    return render(request, 'data_grabber/index.html', context)
+
+def detail(request, player_id):
+    player = get_object_or_404(Player, pk=player_id)
+    return render(request, 'data_grabber/detail.html', {'player': player})
